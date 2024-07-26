30b2021c6f7b3f858e2fbdfec3722bb8cea200fb
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-07-26 20:51:02 +0800

    测试merge 我在master分支上增加了一行内容4

--- src/test/java/org/example/springbootdemo5/Test9.java
+++ src/test/java/org/example/springbootdemo5/Test9.java
@@ -5,5 +5,7 @@ public class Test9 {
         System.out.println("我在add分支上增加了一行内容");
         System.out.println("我在add分支上增加了一行内容2");
         System.out.println("测试merge 我在add分支上增加了一行内容3");
+
+        System.out.println("测试merge 我在add分支上增加了一行内容4");
     }
 }
