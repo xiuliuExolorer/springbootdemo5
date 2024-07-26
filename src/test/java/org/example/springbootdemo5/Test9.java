0b2b3ecb6e83dc29e967e2b0710e14987046e3e6
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-07-26 20:47:37 +0800

    测试merge 我在add分支上增加了一行内容3

--- src/test/java/org/example/springbootdemo5/Test9.java
+++ src/test/java/org/example/springbootdemo5/Test9.java
@@ -4,5 +4,6 @@ public class Test9 {
     public static void main(String[] args) {
         System.out.println("我在add分支上增加了一行内容");
         System.out.println("我在add分支上增加了一行内容2");
+        System.out.println("测试merge 我在add分支上增加了一行内容3");
     }
 }
