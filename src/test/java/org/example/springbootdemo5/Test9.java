728293cbc3962aefa3d4e24e91292e7d10c4d02c
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-07-26 20:44:18 +0800

    add分支 增加 2

--- src/test/java/org/example/springbootdemo5/Test9.java
+++ src/test/java/org/example/springbootdemo5/Test9.java
@@ -3,5 +3,6 @@ package org.example.springbootdemo5;
 public class Test9 {
     public static void main(String[] args) {
         System.out.println("我在add分支上增加了一行内容");
+        System.out.println("我在add分支上增加了一行内容2");
     }
 }
