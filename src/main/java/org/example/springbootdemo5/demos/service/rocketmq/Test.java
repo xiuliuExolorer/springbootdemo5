6589011c9a9aa8d8ac3ea86603bc9ce841c2cd00
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-08-04 18:17:08 +0800

    我增加了一个 sout 2

--- src/main/java/org/example/springbootdemo5/demos/service/rocketmq/Test.java
+++ src/main/java/org/example/springbootdemo5/demos/service/rocketmq/Test.java
@@ -11,5 +11,6 @@ public class Test implements DisposableBean {
     public static void main(String[] args) {
 
         System.out.println("测试回滚git");
+        System.out.println("再次测试回滚");
     }
 }
