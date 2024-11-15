4e7e716675e0f4a8c958faad79ffcf39e749d426
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-11-15 14:28:21 +0800

    增加 test5内容

--- src/main/java/org/example/springbootdemo5/demos/service/collection/Test5.java
+++ src/main/java/org/example/springbootdemo5/demos/service/collection/Test5.java
@@ -1,4 +1,9 @@
 package org.example.springbootdemo5.demos.service.collection;
 
 public class Test5 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
 }
