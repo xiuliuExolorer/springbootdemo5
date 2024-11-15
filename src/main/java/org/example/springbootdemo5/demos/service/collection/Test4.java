aa6b9a0feec34636933bf8a8edeae721cf0f79ef
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-11-15 13:55:13 +0800

    test5终于没有了

--- src/main/java/org/example/springbootdemo5/demos/service/collection/Test4.java
+++ src/main/java/org/example/springbootdemo5/demos/service/collection/Test4.java
@@ -1,4 +1,7 @@
 package org.example.springbootdemo5.demos.service.collection;
 
 public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("test5 终于没有了");
+    }
 }
