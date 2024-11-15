1b947b5517e62225eb702288a707f73cf0978289
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-11-15 14:32:12 +0800

    增加一行文字

--- src/main/java/org/example/springbootdemo5/demos/service/collection/Test4.java
+++ src/main/java/org/example/springbootdemo5/demos/service/collection/Test4.java
@@ -3,5 +3,6 @@ package org.example.springbootdemo5.demos.service.collection;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("难道成功了吗");
+        System.out.println("难道成功了吗");
     }
 }
