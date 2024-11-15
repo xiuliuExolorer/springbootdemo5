334825eb7022544c28da65c858c86244dc66c422
xiuliuExolorer <houmingweigeogle@gmail.com>
2024-11-15 13:04:07 +0800

    1

--- src/main/java/org/example/springbootdemo5/demos/service/collection/Test1.java
+++ src/main/java/org/example/springbootdemo5/demos/service/collection/Test1.java
@@ -1,12 +1,8 @@
 package org.example.springbootdemo5.demos.service.collection;
 
-import java.util.PriorityQueue;
-
 public class Test1
 {
     public static void main(String[] args) {
-        PriorityQueue<Object> objects = new PriorityQueue<>();
-        System.out.println(" master 提交了1");
-        System.out.println(" revert master 提交了55555");
+        System.out.println("11111111");
     }
 }
