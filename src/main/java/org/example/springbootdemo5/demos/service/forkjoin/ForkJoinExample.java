1159cc804a79045fd7a5bc617c25bfc002af6e75
xiuliuExolorer <houmingweigeogle@gmail.com>
2025-03-21 12:39:10 +0800

    four

--- src/main/java/org/example/springbootdemo5/demos/service/forkjoin/ForkJoinExample.java
+++ src/main/java/org/example/springbootdemo5/demos/service/forkjoin/ForkJoinExample.java
@@ -59,6 +59,7 @@ public class ForkJoinExample {
         System.out.println("second commit");
         System.out.println("three commit");
         System.out.println("four");
+        System.out.println("five");
 
 
 //        new BigDecimal()
