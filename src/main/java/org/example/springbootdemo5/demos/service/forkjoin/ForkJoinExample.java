60a2cc74ce20045889909607ae9663c423c78ba8
xiuliuExolorer <houmingweigeogle@gmail.com>
2025-03-21 12:35:44 +0800

    第二次提交

--- src/main/java/org/example/springbootdemo5/demos/service/forkjoin/ForkJoinExample.java
+++ src/main/java/org/example/springbootdemo5/demos/service/forkjoin/ForkJoinExample.java
@@ -56,6 +56,8 @@ public class ForkJoinExample {
             return;
         }
         System.out.println("first commit");
+        System.out.println("second commit");
+
 
 //        new BigDecimal()
 
